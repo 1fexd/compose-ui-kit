b1ee0d106b40c75da780f22075ad5b3eba0170f3
1fexd <PGFvy85nC@protonmail.com>
2026-07-12 17:10:24 +0200

    feat: Extend rememberOptionalContent

--- modules/compose/core/src/main/kotlin/fe/android/compose/content/Content.kt
+++ modules/compose/core/src/main/kotlin/fe/android/compose/content/Content.kt
@@ -7,14 +7,45 @@ public typealias Content = @Composable () -> Unit
 public typealias OptionalContent = (@Composable () -> Unit)?
 
 @Composable
-public inline fun rememberOptionalContent(condition: Boolean, crossinline content: @Composable () -> Unit): OptionalContent {
+public inline fun rememberOptionalContent(
+    condition: Boolean,
+    crossinline content: @Composable () -> Unit
+): OptionalContent {
     return remember(key1 = condition) {
         if (condition) ({ content() }) else null
     }
 }
 
 @Composable
-public inline fun <T : Any?> rememberOptionalContent(key1: T?, crossinline content: @Composable (T) -> Unit): OptionalContent {
+public inline fun <K1 : Any?, K2 : Any?> rememberOptionalContent(
+    condition: Boolean,
+    key1: K1,
+    key2: K2,
+    crossinline content: @Composable () -> Unit
+): OptionalContent {
+    return remember(key1 = key1, key2 = key2) {
+        if (condition) ({ content() }) else null
+    }
+}
+
+@Composable
+public inline fun <K1 : Any?, K2 : Any?, K3 : Any?> rememberOptionalContent(
+    condition: Boolean,
+    key1: K1,
+    key2: K2,
+    key3: K3,
+    crossinline content: @Composable () -> Unit
+): OptionalContent {
+    return remember(key1 = key1, key2 = key2, key3 = key3) {
+        if (condition) ({ content() }) else null
+    }
+}
+
+@Composable
+public inline fun <T : Any?> rememberOptionalContent(
+    key1: T?,
+    crossinline content: @Composable (T) -> Unit
+): OptionalContent {
     return remember(key1 = key1) {
         key1?.let {
             { content(it) }
