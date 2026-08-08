e171a966c8cc03b19d346b20272a7fde158298ed
1fexd <PGFvy85nC@protonmail.com>
2026-08-08 09:50:38 +0200

    chore: Dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,6 @@ pluginManagement {
         id("de.fayard.refreshVersions") version "0.60.6"
         id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
         id("com.android.library")
-        id("org.jetbrains.kotlin.android")
     }
 
     when (val gradleBuildDir = extra.properties["gradle.build.dir"]) {
